fd56f16451f5adc9cd216080755a5dce433c2092
kennynham <knham1@csu.fullerton.edu>
2018-04-21 16:02:10 -0700

    Added ngSwitch and more ngFor

--- Angular/hello-world/src/app/app.component.ts
+++ Angular/hello-world/src/app/app.component.ts
@@ -10,8 +10,24 @@ export class AppComponent {
   post = {
     appFavorite: true
   }
-
   courses = [];
+  viewMode = 'list';
+  list = [
+    { id: 1, name: "Item1" },
+    { id: 2, name: "Item2" },
+    { id: 3, name: "Item3" },
+  ];
+
+  i = 4;
+  onAdd() {
+    this.list.push({id: this.i, name: 'Item'+ this.i});
+    this.i++;
+  }
+
+  onRemove(listItem) {
+    let index = this.list.indexOf(listItem);
+    this.list.splice(index, 1);
+  }
 
   // Passing Data into a known variable, appFavorite
   // onFavoriteChange(appFavorite) {
